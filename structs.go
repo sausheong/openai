2575a75c90b9307e10bcd02da2af82201ca0ac1c
Chang Sau Sheong <sausheong@gmail.com>
2023-03-02 20:41:31 +0800

    added gpt3.5 model

--- structs.go
+++ structs.go
@@ -10,11 +10,12 @@ const (
 	FINETUNES_URL   = "https://api.openai.com/v1/finetunes"
 	MODERATIONS_URL = "https://api.openai.com/v1/moderations"
 
-	TEXT_DAVINCI_003 = "text-davinci-003"
-	TEXT_DAVINCI_002 = "text-davinci-002"
-	TEXT_CURIE_001   = "text-curie-001"
-	TEXT_BABBAGE_001 = "text-babbage-001"
-	TEXT_ADA_001     = "text-ada-001"
+	TEXT_GPT_35_TURBO = "gpt-3.5-turbo"
+	TEXT_DAVINCI_003  = "text-davinci-003"
+	TEXT_DAVINCI_002  = "text-davinci-002"
+	TEXT_CURIE_001    = "text-curie-001"
+	TEXT_BABBAGE_001  = "text-babbage-001"
+	TEXT_ADA_001      = "text-ada-001"
 
 	CODE_DAVINCI_002 = "code-davinci-002"
 	CODE_CUSHMAN_001 = "code-cushman-001"
